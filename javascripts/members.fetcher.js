155f5320f286f6636010ba9887d43b062492df28
Nicholas Ruunu <nicholas@ruu.nu>
2016-05-09 01:05:34 +0200

    Adds member.fether.js to fetch members from gh

--- javascripts/members.fetcher.js
+++ javascripts/members.fetcher.js
@@ -1,3 +1,26 @@
-/**
- * Created by nicholasruunu on 09/05/16.
- */
+jQuery(document).ready(function($) {
+    "use strict";
+
+    var $membersContainer = $('#js-members');
+
+    $.ajax('https://raw.githubusercontent.com/phpse/medlemsregister/master/members.json', {
+        dataType: 'json',
+        success: function(members) {
+            var $memberList = $('<ul/>');
+
+            for (var i in members) {
+                var name =  members[i].name;
+                var avatar = 'https://github.com/' + members[i].github + '.png?size=200';
+
+                $memberList.append(
+                    $('<li/>').append(
+                        $('<img/>').attr('src', avatar),
+                        $('<span/>').text(name)
+                    )
+                );
+            }
+
+            $membersContainer.append($memberList);
+        }
+    });
+});
